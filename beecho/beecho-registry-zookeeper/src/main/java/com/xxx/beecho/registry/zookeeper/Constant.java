264c9eb31a9c5475ea9e1a3a67ddd8bd517c2f19
春哥叨叨 <peter7.zhang@gmail.com>
2017-06-06 13:19:10 +0800

    Update Constant.java

--- beecho/beecho-registry-zookeeper/src/main/java/com/xxx/beecho/registry/zookeeper/Constant.java
+++ beecho/beecho-registry-zookeeper/src/main/java/com/xxx/beecho/registry/zookeeper/Constant.java
@@ -9,5 +9,5 @@ public interface Constant {
     int ZK_SESSION_TIMEOUT = 5000;
     int ZK_CONNECTION_TIMEOUT = 1000;
 
-    String ZK_REGISTRY_PATH = "/beecho_registry";
+    String ZK_REGISTRY_PATH = "/registry";
 }
